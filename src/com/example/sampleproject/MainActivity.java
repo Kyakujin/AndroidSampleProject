ff8a8cb1c781d34880b3e89a8e5ee3b35e59bf27
kyakujin <kyakujin@localhost.localdomain>
2013-08-14 00:34:39 +0900

    first commit

---
+++ src/com/example/sampleproject/MainActivity.java
@@ -0,0 +1,38 @@
+
+package com.example.sampleproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        button = (Button) findViewById(R.id.buttonNext);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == button) {
+            Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(i);
+        }        
+    }
+}
